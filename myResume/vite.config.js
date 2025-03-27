fde318a8f7089b80e48ce2186888347e033e32b3
Mathys FRANCO <mathysf41@gmail.com>
2025-03-27 20:06:18 +0100

    delete vercel file

--- myResume/vite.config.js
+++ myResume/vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-resume-template',
+  base: '/my-resume-web/', // Assure-toi que c'est un chemin absolu
   plugins: [vue()],
 })
